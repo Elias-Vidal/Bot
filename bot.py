2bb2ed3f90179c677c195d8a6a1dd9c5c088bb55
Elias <eliasjuniorfranca@hotmail.com>
2024-12-12 23:09:02 -0300

    atualizacao codigo

--- bot.py
+++ bot.py
@@ -1,1 +1,28 @@
-import selenium
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+import os
+import time
+import requests
+
+#API 
+agent = {"User-Agent": 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'}
+
+api = requests.get("https://editacodigo.com.br/index/api-whatsapp/x0YJ8Cww908JtO6LTJmQAvaCVkz4sNXk" ,  headers=agent)
+time.sleep(1)
+api = api.text
+api = api.split(".n.")
+bolinha_notificacao = api[3].strip()
+contato_cliente = api[4].strip()
+caixa_msg = api[5].strip()
+msg_cliente = api[6].strip()
+caixa_msg2 = api[7].strip()
+caixa_pesquisa = api[8].strip()
+
+dir_path = os.getcwd()
+chrome_options2 = Options()
+chrome_options2.add_argument(r"user-data-dir=" + dir_path + "/pasta/sessao")
+driver = webdriver.Chrome(chrome_options=chrome_options2)
+driver.get('https://web.whatsapp.com/')
+time.sleep(120)
